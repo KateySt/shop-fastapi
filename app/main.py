9fead14a7106892d01f539006fe93e5b657f39c0
KateySt <kateystetsenko@gmail.com>
2026-03-15 18:53:24 +0200

    add nginx

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.config import app_config
 from app.exception import register_exception_handlers
 from app.routers import company_router, item_router
 
-app = FastAPI(title=app_config.APP_NAME)
+app = FastAPI(title=app_config.APP_NAME, root_path="/api")
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,5 +17,5 @@ app.add_middleware(
 
 register_exception_handlers(app)
 
-app.include_router(company_router, prefix="/api/companies", tags=["companies"])
-app.include_router(item_router, prefix="/api/items", tags=["items"])
+app.include_router(company_router, prefix="/companies", tags=["companies"])
+app.include_router(item_router, prefix="/items", tags=["items"])
